b8d0e493308670a78571b06386b6db1664d466f3
David Renshaw <dwrenshaw@gmail.com>
2014-07-16 15:48:14 +0000

    publishing sorta almost works

--- wp-content/plugins/sandstorm/publish-it.sh
+++ wp-content/plugins/sandstorm/publish-it.sh
@@ -2,13 +2,9 @@
 STATICDIR=/var/www
 TMPDIR=/var/www_`date +%s%N`
 URL=http://127.0.0.1:10000/
-REPLACE_URL=http://this_will_be_replaced_by_sandstorm
 
 rm -rf $TMPDIR
-wget -r -p -np -nH -U 'sandstormpublish' -P $TMPDIR $URL
-find $TMPDIR -name '*\?*' -print0 | xargs -0 -n1 bash -c 'echo "moving $0 to ${0%\?*}";mv "$0" "${0%\?*}"'
-find $TMPDIR -type f -exec sed -i "s|${REPLACE_URL}/|/|g" {} \;
-find $TMPDIR -type f -exec sed -i "s|${REPLACE_URL}|/|g" {} \; # URLs without a trailing / need to be changed to point the index
+wget --mirror -p --html-extension --convert-links -nH -P $TMPDIR $URL
 
 mv $STATICDIR ${STATICDIR}_tmp
 mv $TMPDIR $STATICDIR
