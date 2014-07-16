b37853213f7a7d57e8dc5b2eb8e99d09c4fd0ab7
David Renshaw <dwrenshaw@gmail.com>
2014-07-16 02:30:25 +0000

    display publishing info

--- wp-content/plugins/sandstorm/sandstorm.php
+++ wp-content/plugins/sandstorm/sandstorm.php
@@ -19,3 +19,24 @@ function sandstorm_publish() {
 
 add_action('publish_post', 'sandstorm_publish');
 
+
+function sandstorm_publishing_info() {
+  ?>
+  <p> To set up your domain to point at your published site,
+  add the following DNS records to your domain. Replace host.example.com with your site's hostname.
+  </p>
+  <p/>
+  <?php
+  $lines = array();
+  $result = exec('/sandstorm/bin/getPublicId', $lines);
+  echo "<p> host.example.com IN CNAME $lines[1] </p>";
+  echo "<p> sandstorm-www.host.example.com IN TXT $lines[0] </p>";
+  ?>
+  <p/>
+  <p>
+Note: If your site may get a lot of traffic, consider putting it behind a CDN.
+  </p>
+  <?php
+}
+
+add_action('rightnow_end', 'sandstorm_publishing_info');
