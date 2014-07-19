d77b6dcf37e36242d74d8bc5392478806b50f7cf
David Renshaw <dwrenshaw@gmail.com>
2014-07-19 02:19:43 +0000

    drop-in install.php with no initial posts or comments

---
+++ wp-content/install.php
@@ -0,0 +1,10 @@
+<?php
+
+function wp_install_defaults() {
+         // Set up default widgets for default theme.
+         update_option( 'widget_recent-posts', array ( 2 => array ( 'title' => '', 'number' => 5 ), '_multiwidget' => 1 ) );
+         update_option( 'widget_archives', array ( 2 => array ( 'title' => '', 'count' => 0, 'dropdown' => 0 ), '_multiwidget' => 1 ) );
+         update_option( 'sidebars_widgets', array ( 'wp_inactive_widgets' => array (), 'sidebar-1' => array ( 0 => 'search-2', 1 => 'recent-posts-2', 2 => 'recent-comments-2', 3 => 'archives-2', 4 => 'categories-2', 5 => 'meta-2', ), 'sidebar-2' => array (), 'sidebar-3' => array (), 'array_version' => 3 ) );
+
+
+}
