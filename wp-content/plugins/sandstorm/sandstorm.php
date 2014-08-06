5a9201e2c9b8f3bc3191794e3c53e0e0fb8fda87
David Renshaw <dwrenshaw@gmail.com>
2014-08-06 13:43:39 +0000

    remove shortlink and other unneeded things

--- wp-content/plugins/sandstorm/sandstorm.php
+++ wp-content/plugins/sandstorm/sandstorm.php
@@ -156,3 +156,25 @@ function sandstorm_search_form($orig) {
       return $form;
    }
 }
+
+/* -------------
+We include this tiny plugin inline:
+
+PLUGIN NAME: Remove WP version and shortlink
+PLUGIN URL:http://wordpress.org/extend/plugins/remove-wp-version-and-shortlink/
+DESCRIPTION: Removes Wordpress and version and short link
+AUTHOR: Naganandhini
+AUTHOR EMAIL: me@naganandhini.com
+AUTHOR URI:http://www.naganandhini.com/
+*/
+
+/*To remove RSD Link*/
+remove_action('wp_head', 'rsd_link');
+/*To remove WLW Link*/
+remove_action('wp_head', 'wlwmanifest_link');
+/*To remove wordpress version*/
+remove_action('wp_head', 'wp_generator');
+/*To remove shortlink*/
+remove_action('wp_head', 'wp_shortlink_wp_head');
+
+// ---------------
